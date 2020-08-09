6121ddfa310370ae4d60b2d3fde51ea41ccd55c5
josegpt27@gmail.com <Jose G Perez Taveras>
2020-08-09 14:06:26 -0400

    fix 404 error page

--- app/pages/404.js
+++ app/pages/404.js
@@ -3,14 +3,11 @@ import c from "../helpers"
 
 function Custom404() {
   return (
-    <section className="flex flex-col items-center capitalize justify-center">
+    <section className="flex flex-col items-center justify-center flex-1 capitalize">
       <h1 className="flex">
         <div className="flex items-center mr-4 text-gray-200">
           <span className="relative flex w-3 h-3">
-            <span
-              className="absolute inline-flex w-full h-full bg-red-400 rounded-full
-            opacity-75 animate-ping"
-            />
+            <span className="absolute inline-flex w-full h-full bg-red-400 rounded-full opacity-75 animate-ping" />
             <span className="relative inline-flex w-3 h-3 bg-red-500 rounded-full" />
           </span>
           <span className="text-6xl text-red-500">404</span>
