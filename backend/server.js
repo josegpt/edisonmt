5dc9c30fb77b063d6499f88702d1159588a7eadf
Jose G Perez Taveras <josegpt27@gmail.com>
2020-08-25 12:41:33 -0400

    added server

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,52 @@
+const http = require("http")
+const axios = require("axios")
+const _ = require("lodash")
+const { xml2json } = require("xml-js")
 const express = require("express")
 const bodyParser = require("body-parser")
 
 const app = express()
+const server = http.createServer(app)
+const io = require("socket.io").listen(server)
 const RTMP_SECRET = process.env.RTMP_SECRET || "dev"
 
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use((req, res, next) => {
+  req.io = io
+  next()
+})
 
-app.post("/auth", (req, res) => {
+async function fetchStreams() {
+  try {
+    const response = await axios.get("https://edisonmt.com/stats")
+    const result = xml2json(response.data, { compact: true, spaces: 2 })
+    const data = JSON.parse(result)
+    const streams = data.rtmp.server.application.live.stream
+    return _.isArray(streams) ? streams : streams ? [streams] : []
+  } catch (err) {
+    return err
+  }
+}
+
+async function fetchStream() {
+  try {
+    const response = await axios.get("https://edisonmt.com/stats")
+    const result = xml2json(response.data, { compact: true, spaces: 2 })
+    const data = JSON.parse(result)
+    const streams = data.rtmp.server.application.live.stream
+    return _.isArray(streams) ? streams : streams ? [streams] : []
+  } catch (err) {
+    return err
+  }
+}
+
+app.post("/auth", async (req, res) => {
   if (req.body.streamKey === RTMP_SECRET) {
     res.sendStatus(201)
+    req.io.emit("streams", await fetchStreams())
   } else {
     res.sendStatus(404)
+    req.io.emit("streams", await fetchStreams())
   }
 })
 
@@ -18,6 +54,16 @@ app.get("*", (req, res) => {
   res.sendStatus(401)
 })
 
-app.listen(3000, () => {
+io.on("connection", (socket) => {
+  console.log(`Connected socket ${socket.id}`)
+
+  socket.on("disconnect", () => console.log(`Disconnected socket ${socket.id}`))
+
+  socket.on("streams", async () => {
+    socket.emit("streams", await fetchStreams())
+  })
+})
+
+server.listen(3000, () => {
   console.log("Server started at http://localhost:3000")
 })
