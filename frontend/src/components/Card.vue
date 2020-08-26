b8d2f04b5f2b5035d04dc048fa08c6ba14257221
Jose G Perez Taveras <josegpt27@gmail.com>
2020-08-26 13:18:33 -0400

    add card

--- frontend/src/components/Card.vue
+++ frontend/src/components/Card.vue
@@ -4,9 +4,9 @@
     class="relative block px-4 py-12 text-center text-gray-500 bg-gray-800 rounded hover:text-gray-600 hover:bg-gray-900"
   >
     <span class="text-2xl font-semibold capitalize">{{ title }}</span>
-    <!-- <span class="absolute top-0 right-0 p-4 text-xs font-semibold lowercase">
+    <span class="absolute top-0 right-0 p-4 text-xs font-semibold lowercase">
       {{ viewers }} {{ viewers === 1 ? "espectador" : "espectadores" }}
-    </span>-->
+    </span>
   </router-link>
 </template>
 
